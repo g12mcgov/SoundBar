58a4dafc4c1670de08ac05b8026f643ff3e8df43
Grant McGovern <grant@cobrain.com>
2015-05-26 23:15:25 -0400

    Beginning to add SoundCloud auth & my user profile

--- soundbar.js
+++ soundbar.js
@@ -1,7 +1,29 @@
-var menubar = require('menubar')
+var menubar = require('menubar');
+var Menu = require('menu');
 
-var mb = menubar({dir: __dirname})
+var mb = menubar({dir: __dirname});
+
+var menu_template = [{
+	label: 'SoundBar',
+    submenu: [
+      {
+        label: 'Quit',
+        selector: 'quitSoundBar'
+      },
+      {
+        type: 'separator'
+      }
+    ]
+}];
 
 mb.on('ready', function ready () {
-  console.log('app is ready')
+	console.log("Running SoundBar");
+
+	var menu = new Menu();
+	var menu = Menu.buildFromTemplate(menu_template);
+	Menu.setApplicationMenu(menu); // Must be called within app.on('ready', function(){ ... });
+	
+	//mb.app.quit();
 })
+
+
